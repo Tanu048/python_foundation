d73e106ce984989876b0e77de796dea10b3aa934
Tanu048 <tanu.patidar.d@gmail.com>
2025-12-19 19:55:08 +0530

    Updated Structure

---
+++ 01_basics/guessing_game/guess_game.py
@@ -0,0 +1,25 @@
+import random as rd
+
+print("\t\tTHE GUESSING GAME !!!")                             #\t is the same for a tab space
+
+upper_bond=int(input("Enter a number:"))
+
+if upper_bond>=0:                                        
+    random_number=rd.randint(0,upper_bond)
+else:
+    print("Type an integer greater than 0 next time!")
+    quit()                                                            # ends the program 
+
+while True:
+    user_input=input("make a guess: ")
+    if user_input.isdigit():
+        user_input=int(user_input)
+        if user_input>=0:
+            if user_input==random_number:
+                print("You guessed it right, you win!!")
+                break
+            else:
+                print("Not a match, make a choice again.")
+                continue
+    else:
+        print("Type an integer greater than 0 next time!")   
