f140b5a6ec9b5d6f74dd7ef54a942571754599c3
Tanu048 <tanu.patidar.d@gmail.com>
2026-01-05 23:52:09 +0530

    Added new function

--- 04_intermediate/student_management/students.py
+++ 04_intermediate/student_management/students.py
@@ -1,56 +1,95 @@
 # a student management system using nested dictonaries and file methods
 
 file="student_log.txt"
-students={}
+
+#  add the load function to get data from file into dictonary and save function to save data from dictonary to file as when program runs it uses dict and not file hence both load and save are important 
+
+def load_data():
+   students={}
+   with open(file,"w+") as f:
+         for line in f:
+            sclass,sroll,sname,smarks =line.strip().split(",")
+            students.setdefault(sclass,{})
+            students[sclass][sroll]={"name":sname,"marks":smarks}
+   return students
+   
+def save_data(students):
+    with open(file, "a+") as f:
+        for cls in students:
+            for roll in students[cls]:
+                s = students[cls][roll]
+                f.write(f"{cls},{roll},{s['name']},{s['marks']}\n")
 
 def add_student():
     sclass=int(input('Enter class: '))
     sroll=int(input('Enter roll number: '))
     sname=(input('Enter name: '))
     smarks=int(input('Enter marks: '))
-    
+    students={sclass:{sroll:{"name":sname,"marks":smarks}}}
     if sclass not in students:
        students[sclass]={}
 
-    students[sclass][sroll]={"name":sname,"marks":smarks}
-    with open(file,"a+") as f:               # the functions in string methods take only string, list and int data types
-       f.write(f"Class: {sclass}\t Roll Number: {sroll}\t  Name: {sname.title()}\t  Marks: {smarks}\n")
+    students[sclass][sroll]={"name":sname.title(),"marks":smarks}
+    save_data(students)
 
 
 def view_data(students):
-   with open(file,"r") as f:
-      for line in f:
-         if not line:
-            print("No data!")
-         print(f"{line}\n")
+   for cls in students:
+      for roll,s in students[cls].items():         #here roll is a key and s is a value so roll,s is a key value pair in students
+         print(f"Class: {cls}\tRoll Number: {roll}\tName: {s['name']}\tMarks: {s['marks']}\n")
 
 
-def searchByClass(students):
-   sclass=int(input("Enter class: "))
-   for Class in students:
-      if Class==sclass:
-         print(students)
+def searchByRoll(students):
+   roll=(input("Enter roll number: "))
+   for cls in students:
+      if roll in students[cls]: 
+         print(f"Found {students[cls][roll]}")
+      else:
+         print("Student not found.")
+         
+def searchByName(students):
+   name=(input("Enter name: "))
+   for cls in students:
+      for roll in students[cls]:
+         if name.title() in students[cls][roll]["name"]: 
+            print(f"Found {cls},{{{roll}}}{students[cls][roll]}")
+         else:
+            print("Student not found.")
 
 
+def delete(students):
+   cls=input("Enter the class:")
+   roll=input("Enter the roll number:")
+   if cls in students and roll in students[cls]:
+      del students[cls][roll]
+   else:
+      print("entry not found.")
+   save_data(students)
+   
             
 def main():
-   print("Welcome to student management system!!!\n")
-   choice=int(input('choose:\n1. to add a student\n2. to view list\n3. to search a student\n\ta. by roll number\n\tb. by class\n4. to delete a student\n5. to exit '))
+   students=load_data()
    flag=0
+   print("Welcome to student management system!!!\n")
    while (flag != -1):
+      choice=int(input('choose:\n1. to add a student \n2. to view list \n3. to search a student \n\ta.By ROll Number \n\tb.By Name   \n4. to delete a student\n5. to exit '))
       match choice:
          case 1:
             add_student()
          case 2:
             view_data(students)
          case 'a':
-            pass
-         case "b":
-            pass
+            searchByRoll(students)
+         case 'b':
+            searchByName(students)
          case 4:
-            pass
+            delete(students)
          case 5:
             flag=-1
          case _:
-            pass
- 
+            print("Enter valid inputs!!!")
+
+students=load_data()
+add_student()
+add_student()
+delete(students)
